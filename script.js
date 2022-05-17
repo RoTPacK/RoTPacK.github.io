6e6afc2a6f1cc82e8316369645aa91d7117e233b
RoTPacK <103390006+RoTPacK@users.noreply.github.com>
2022-05-17 21:07:05 +0200

    change code

--- script.js
+++
@@ -1,7 +0,0 @@
-function policz(){
-    var powierzchnia = document.getElementById("powierzchnia").value;
-    var wynik = document.getElementById("wycena");
-    var puszki = Math.ceil(powierzchnia/4);
-
-    wynik.innerHTML = "Liczba jednolitrowych puszek farby potrzebnych do pomalowania wynosi: " +puszki; 
-}
